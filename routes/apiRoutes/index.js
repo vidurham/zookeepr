d5b30e59ecda949eb4e697a667ca9d088ac1b5d9
Vinny D <vdurham@prideseals.com>
2022-03-30 11:28:53 -0400

    finished

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const animalRoutes = require('../apiRoutes/animalRoutes');
 
 router.use(animalRoutes);
+router.use(require('../apiRoutes/zookeeperRouteszookeeperRoutes'));
 
 module.exports = router;
